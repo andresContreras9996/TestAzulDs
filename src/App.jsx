20dd08a7bb1783e78de4c56927e585f9fa366a31
andresContreras9996 <andres.contreras@theksquaregroup.com>
2024-12-23 10:52:13 -0600

    test libray

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
+import { AzulButton, AzulStepper } from '@the-ksquare-group/azul-ds';
 import './App.css';
+import React from 'react';
+
 
 function App() {
+
+  const options = [
+    'option 1', 'option2', 'option3', 'test', 'test2'
+  ]
+
+  const onClickButton = (e) => {
+    e.currentTarget.blur()
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +22,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div style={{ width: '100%' }}>
+          <AzulStepper steps={options} activeStep={3} />
+          <AzulButton variant="primary" label='Hello' onClick={onClickButton}></AzulButton>
+
+        </div>
+
+
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { setConfig } from '@the-ksquare-group/azul-ds';
+import theme from './theme.json'
+
+setConfig(theme)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
